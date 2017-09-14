fec8ea8400c6dcae323f3675282b3c452d81c69f
Nicolas Gnyra <nicolas@gnyra.com>
2017-09-13 23:03:49 -0400

    Format & animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from './app.component';
 import {AssetGroupsComponent} from './asset-groups.component';
-// import {AppRoutingModule} from './app-routing.module';
 
 import {TagFilterPipe} from './tag-filter.pipe';
 
@@ -15,8 +15,8 @@ import {TranslationSortPipe} from './translation-sort.pipe';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    // AppRoutingModule,
     HttpModule,
     NgbModule.forRoot()
   ],
--- src/app/asset-groups.component.ts
+++ src/app/asset-groups.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {trigger, state, style, animate, transition, query} from '@angular/animations';
 
 import {DataService} from './data.service';
 
@@ -6,16 +7,36 @@ import {DataService} from './data.service';
   selector: 'app-asset-groups',
   templateUrl: './asset-groups.component.html',
   styleUrls: ['./asset-groups.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('paneState', [
+      state('active', style({
+        opacity: 1,
+        display: 'block'
+      })),
+      state('inactive', style({
+        opacity: 0,
+        display: 'none'
+      })),
+      transition('inactive => active', animate('300ms ease')),
+      transition('active => inactive', animate('300ms ease'))
+    ])
+  ]
 })
 export class AssetGroupsComponent implements OnInit {
   constructor(public dataService: DataService) {}
 
   data: Object;
+  activeId: string;
 
   ngOnInit(): void {
     this.dataService.loadData().then(data => {
       this.data = data;
     });
+  }
+
+  select(tabId): void {
+    console.log(tabId);
+    this.activeId = tabId;
   }
 }
