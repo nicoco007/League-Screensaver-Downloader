9272ff3c1ac9f480ad382e43bc7fc5c24c581daf
Nicolas Gnyra <nicolas@gnyra.com>
2018-04-24 19:02:34 -0400

    Clean up constructor

--- src/app/collection.ts
+++ src/app/collection.ts
@@ -1,7 +1,12 @@
 import {Asset} from './asset';
 
 export class Collection {
-  constructor(public id: string, public nameTranslateId: string, public cover: Asset, public categories: string[], public tags: string[], public assets: Asset[], public featured: boolean) {
-
-  }
+  constructor(
+    public id: string,
+    public nameTranslateId: string,
+    public cover: Asset,
+    public categories: string[],
+    public tags: string[],
+    public assets: Asset[],
+    public featured: boolean) { }
 }
