b1be39b7824ea534e5efbf04eede27a09d9dcce6
Nicolas Gnyra <nicolas@gnyra.com>
2018-04-25 15:59:28 -0400

    Minor adjustments to translation sort pipe

--- src/app/translation-sort.pipe.ts
+++ src/app/translation-sort.pipe.ts
@@ -1,21 +1,22 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {DataService} from './data.service';
+import {Translatable} from './translatable';
 
 @Pipe({
-  name: 'translationsort',
+  name: 'translationSort',
   pure: false
 })
 export class TranslationSortPipe implements PipeTransform {
   constructor(private dataService: DataService) {}
 
-  transform(value: any, ...args: any[]): any[] {
+  transform(value: Translatable[], ...args: any[]): any[] {
     if (!value || !args) {
       return args;
     }
 
     return value.sort((a, b) => {
-      const strA = this.dataService.translate(a['nameTranslateId']);
-      const strB = this.dataService.translate(b['nameTranslateId']);
+      const strA = this.dataService.translate(a.nameTranslateId);
+      const strB = this.dataService.translate(b.nameTranslateId);
 
       if (!strA || !strB) {
         return 0;
